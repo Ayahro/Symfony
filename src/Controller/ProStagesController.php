ff65aba03326e886c854e07aee875c6ed8b8ea30
Soraya DIHANE <sdihane@iutbayonne.univ-pau.fr>
2021-01-09 14:30:54 -0800

    Début du TP2

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -11,31 +11,39 @@ class ProStagesController extends AbstractController
     /**
      * @Route("/", name="proStages_Accueil")
      */
-    public function index_accueil(): Response
+    public function accueil(): Response
     {
         return $this->render('pro_stages/index.html.twig');
     }
+
+
     /**
        * @Route("/entreprises", name="proStages_Entreprises")
        */
-      public function index_entreprises(): Response
+      public function entreprises(): Response
       {
           return $this->render('pro_stages/index_entreprises.html.twig');
       }
 
+
       /**
          * @Route("/formations", name="proStages_Formations")
          */
-        public function index_formations(): Response
+        public function formations(): Response
         {
             return $this->render('pro_stages/index_formations.html.twig');
         }
+
+
         /**
-           * @Route("stages/1", name="proStages_stages")
+           * @Route("/stages/{id}", name="proStages_stages")
            */
-          public function index_stages(): Response
+          public function stages($id): Response
           {
-              return $this->render('pro_stages/index_stages.html.twig');
+              return $this->render('pro_stages/index_stages.html.twig',
+            ['idStages' => $id ]);
 
           }
+
+
 }
